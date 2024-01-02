50f7dd7acb6a40d59fe7c9792824cb37ecccef97
azriki97 <azriki97>
2024-01-03 00:13:51 +0700

    Page 4

---
+++ src/components/shape/segitiga1.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="462" height="819" viewBox="0 0 462 819" fill="none">
+        <path d="M0 819L6.10352e-05 0L462 0L0 819Z" fill="#2D2D2D"/>
+    </svg>
+</template>
---
+++ src/components/shape/segitiga2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="triangle-background">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1333" height="2342" viewBox="0 0 1333 2342" fill="none">
+          <path d="M1333 0L1333 2342H0L1333 0Z" fill="#2D2D2D"/>
+      </svg>
+    </div>
+  </template>
+  
+  <style scoped>
+  .triangle-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+  </style>
